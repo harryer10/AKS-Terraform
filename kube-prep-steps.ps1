98e1ba5491d7f57f75b2bc46e326400b90860058
harryer10 <harryer10@hotmail.com>
2022-11-20 15:26:45 +0800

    update env variable

--- kube-prep-steps.ps1
+++ kube-prep-steps.ps1
@@ -1,15 +1,15 @@
 #### Parameters
 
-$keyvaultname = "xxxxx"
-$location = "xxxxx"
-$keyvaultrg = "xxxxxx"
-$sshkeysecret = "xxxxx"
-$spnclientid = "xxxxxx"
-$clientidkvsecretname = "xxxxxx"
-$spnclientsecret = "xxxxxx"
-$spnkvsecretname = "xxxxx"
-$spobjectID = "xxxxx"
-$userobjectid = "xxxxx"
+$keyvaultname = "aks-harryer10-cluster-kv"
+$location = "southeastasia"
+$keyvaultrg = "aks-harryer10-rg"
+$sshkeysecret = "akssshpubkey"
+$spnclientid = "996bbb48-b268-4b01-a45c-7b4ccbb2961b"
+$clientidkvsecretname = "spn-id"
+$spnclientsecret = "i-t8Q~4D1vu6k0bwICZdHIGTOmtiTnjrdx5FpaqL"
+$spnkvsecretname = "spn-secret"
+$spobjectID = "996bbb48-b268-4b01-a45c-7b4ccbb2961b"
+$userobjectid = "996bbb48-b268-4b01-a45c-7b4ccbb2961b"
 
 
 #### Create Key Vault
